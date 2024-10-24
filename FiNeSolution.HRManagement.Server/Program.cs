eed0ae91dba9db016516272d7105b47a2d7e4805
dimitrijevicfilip995@outlook.com <dimitrijevicfilip995@outlook.com>
2024-10-24 19:47:06 +0200

    Added field for Bearer token in Swagger

--- FiNeSolution.HRManagement.Server/Program.cs
+++ FiNeSolution.HRManagement.Server/Program.cs
@@ -1,6 +1,8 @@
 using Core.Application;
 using Core.Infrastructure.Identity;
 using Core.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,7 +22,31 @@ builder.Services.AddCors(options =>
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition(name: JwtBearerDefaults.AuthenticationScheme, securityScheme: new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Description = "Enter the Bearer Authorization : `Bearer Generated-JWT-Token`",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+        new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = JwtBearerDefaults.AuthenticationScheme
+            }
+        }, new String[]{ }
+        }
+});
+});
 
 var app = builder.Build();
 
